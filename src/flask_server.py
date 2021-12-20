74a7ec560bbc466d050a79ec7355bd9b07541888
g_farida <faridafarida378@gmail.com>
2021-12-20 03:53:05 +0300

    Deleted test server

--- src/flask_server.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-@app.route('/test')
-def get_index():
-    return '<html><center><script>document.write("Hello!")</script></center></html>'
